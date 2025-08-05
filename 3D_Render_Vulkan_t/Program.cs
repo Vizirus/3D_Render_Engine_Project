cd326e21d11ada9f98d8026c4d081513be37751e
artem <denloken@gmail.com>
2025-08-05 20:59:41 +0300

    Added Vulkan`s test application

---
+++ 3D_Render_Vulkan_t/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3D_Render_Vulkan_t
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
