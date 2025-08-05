6f40fef941fdfecb7459866d8ac79ba30c3a1737
artem <denloken@gmail.com>
2025-08-05 20:16:12 +0300

    Add project files.

---
+++ 3D_Render_OpenGL_t/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _3D_Render_OpenGL_t
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
